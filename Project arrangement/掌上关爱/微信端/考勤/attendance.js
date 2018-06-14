75738adaee04e25a7927bc9e6f41195664bd4672
llk-90 <33407076+llk-90@users.noreply.github.com>
2018-06-14 17:15:45 +0800

    add
    add

--- Project arrangement/掌上关爱/微信端/考勤/attendance.js
+++ Project arrangement/掌上关爱/微信端/考勤/attendance.js
@@ -14,7 +14,6 @@
 
  //网络请求
   function  request(){
-     // var xval=getBusyOverlay('viewport',{color:'white', opacity:0.75, text:'加载中', style:'text-shadow: 0 0 3px black;font-weight:bold;font-size:16px;color:white'},{color:'#59bf6b', size:70, type:'o'});
       $.ajax(baseURL+"/weixiplusCommon/getAtteinfo.webapp", {
          data:{
              openId:userInfo.openId
@@ -24,15 +23,10 @@
          timeout: 10000, //超时时间设置为10秒；
          crossDomain:true,
          async:false,
-        /* beforeSend:function(){
-             if(xval) {
-                 xval.settext("加载中，请稍后......");
-             }
-         },*/
+      
          success: function(data) {
              //成功回调
              if (data.errorcode.code==0){//成功时显示全部
-                // xval.remove();
                  configView(data)
              }else if (data.errorcode.code==1003)
              {//仅没有考勤信息时，显示用户信息
@@ -57,26 +51,34 @@
   function  configView(data){
       document.getElementById("username").innerHTML=data.infolist[0].StuName;
       document.getElementById("classname").innerHTML=data.infolist[0].ClassName;
-      document.getElementById("latecount").innerHTML= typeof (data.infolist[0].lateCount)=="undefined"?"0":data.infolist[0].lateCount;
-      document.getElementById("leavecount").innerHTML= typeof (data.infolist[0].leaveCount)=="undefined"?"0":data.infolist[0].leaveCount;
+      $("#kaoqin").text(data.infolist[0].StuName+"一周的考勤记录如下");
+      // document.getElementById("latecount").innerHTML= (data.infolist[0].lateCount)=="undefined"?"0":data.infolist[0].lateCount;
+     // document.getElementById("leavecount").innerHTML= (data.infolist[0].leaveCount)=="undefined"?"0":data.infolist[0].leaveCount;
       if (data.attelist.length==0) return;
       var table = document.body.querySelector('#list-view');
-      for (var  i = 0;i < data.attelist.length;i ++) {
-          var li = document.createElement('li');
-          if (data.attelist[i]!= "") {
-        	  if(data.attelist[i+1] == null){
-        		  data.attelist[i+1]=0;
-        	  }
-              li.innerHTML = '<li  class=' + '> <a href="attendance_Ranking_List.html?querydate=' + '&openid=' + userInfo.openId + '"> <i>' + data.attelist[i].substring(0,2) + '日</i> <span>' + data.attelist[i+1] + '</span><p><b>上午</b>' + data.attelist[i].substring(3,11) + '到 ~ ' + data.attelist[i].substring(15,23) + ' 离</p><p><b>下午</b>'+data.attelist[i].substring(27,35)+'到 ~ '+data.attelist[i].substring(39)+' 离</p> </a> </li>';
-                i++;
-          }
-          else {
-          }
-
-          //下拉刷新，新纪录插到最前面；
-
-          table.appendChild(li);
-      }
+    	  for (var  i = 0;i < data.attelist.length;i ++) {
+    		  var li = document.createElement('li');
+    		  if (data.attelist[i]!= "") {
+    			  if(data.attelist[i+1] == null){
+    				  data.attelist[i+1]=0;
+    			  }
+    			  var amin = data.attelist[i].substring(3,11)//上午到
+    			  var amout = data.attelist[i].substring(15,23)//上午离
+    			  var pmin = data.attelist[i].substring(27,35)//下午到
+    			  var pmout = data.attelist[i].substring(39)//下午离
+    		     if(amin.trim()=="00:00:00"){
+    		    	 amin = "-- : -- : --"; 
+    		     }
+    			  if(pmout.trim()=="00:00:00"){
+    				  pmout ="-- : -- : --"
+    			  }
+    			li.innerHTML = ' <a href="attendance_Ranking_List.html?querydate='+data.attelist[i+2] + '&openid=' + userInfo.openId + '"> <i>' + data.attelist[i].substring(0,2) + '日</i> <span>' + data.attelist[i+1] + '</span><p><b>到校时间</b>' + amin + '</p><p><b>离校时间</b>'+pmout+'</p> </a>';
+    			  i+=2;
+    		  }
+    		  else {
+    		  }
+    		  table.appendChild(li);
+    	  }
   }
    function nextPage(a,b){
        window.location.href = 'attendance_Ranking_List.html?querydate='+ a+'&openid='+b;
